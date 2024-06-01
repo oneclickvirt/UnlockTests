db38f4437850c2fd9c9376ab52df437d34089c0a
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-06-01 02:11:12 +0000

    update

--- au/KayoSports.go
+++ au/KayoSports.go
@@ -2,10 +2,11 @@ package au
 
 import (
 	"fmt"
-	"github.com/oneclickvirt/UnlockTests/model"
-	"github.com/parnurzeal/gorequest"
 	"strings"
 	"time"
+
+	"github.com/oneclickvirt/UnlockTests/model"
+	"github.com/parnurzeal/gorequest"
 )
 
 // KayoSports
@@ -16,10 +17,10 @@ func KayoSports(request *gorequest.SuperAgent) model.Result {
 	request = request.Set("User-Agent", model.UA_Browser).Timeout(20 * time.Second)
 	resp, _, errs := request.Get(url).Retry(2, 5).End()
 	if len(errs) > 0 {
-		if strings.Contains(errs[0].Error(), "i/o timeout") {
-			return model.Result{Name: name, Status: model.StatusNo}
-		}
-		return model.Result{Name: name, Status: model.StatusNetworkErr, Err: errs[0]}
+		// if strings.Contains(errs[0].Error(), "i/o timeout") {
+		return model.Result{Name: name, Status: model.StatusNo}
+		// }
+		// return model.Result{Name: name, Status: model.StatusNetworkErr, Err: errs[0]}
 	}
 	defer resp.Body.Close()
 	if strings.Contains(resp.Header.Get("Set-Cookie"), "geoblocked=true") {
