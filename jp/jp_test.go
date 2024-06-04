4208523a9a134982cad86e11cbd4d0f33c5d330b
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-06-04 13:56:36 +0000

    update

--- jp/jp_test.go
+++ jp/jp_test.go
@@ -2,8 +2,9 @@ package jp
 
 import (
 	"fmt"
-	"github.com/oneclickvirt/UnlockTests/utils"
 	"testing"
+
+	"github.com/oneclickvirt/UnlockTests/utils"
 )
 
 func Test(t *testing.T) {
@@ -15,153 +16,153 @@ func Test(t *testing.T) {
 	}
 	fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Kancolle(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Kancolle(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = KaraokeDam(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = KaraokeDam(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = KonosubaFD(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = KonosubaFD(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = PCRJP(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = PCRJP(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = PrettyDerby(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = PrettyDerby(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Radiko(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Radiko(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = RakutenTV(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = RakutenTV(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = TVer(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = TVer(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Telasa(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Telasa(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = UNext(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = UNext(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = VideoMarket(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = VideoMarket(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Abema(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Abema(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = DMM(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = DMM(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = DMMTV(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = DMMTV(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = FOD(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = FOD(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Hulu(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Hulu(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Niconico(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Niconico(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = ProjectSekai(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = ProjectSekai(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = WorldFlipper(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = WorldFlipper(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Wowow(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Wowow(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Lemino(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Lemino(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = Mora(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = Mora(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = EroGameSpace(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = EroGameSpace(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = MusicBook(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = MusicBook(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 
-	res = DAnimeStore(req)
-	if res.Err != nil {
-		fmt.Println(res.Err)
-	}
-	fmt.Println(res.Name, ": ", res.Status, res.Region)
+	// res = DAnimeStore(req)
+	// if res.Err != nil {
+	// 	fmt.Println(res.Err)
+	// }
+	// fmt.Println(res.Name, ": ", res.Status, res.Region)
 }
